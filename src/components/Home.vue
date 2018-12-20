65dd747cce996fba44b4732a1ca314348f357ecd
Pelang <phelang.qhu@gmail.com>
2018-12-20 14:18:24 +0200

    BSL001 - Create home page, Add Bootstrap Vue, Add Material Design Bootstrap

---
+++ src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="Home">
+    <h1>{{ msg }}</h1>
+    <br/>
+    <br/><br/><br/>
+    <b-alert show variant="primary">Primary Alert</b-alert>
+    <b-alert show variant="secondary">Secondary Alert</b-alert>
+    <b-alert show variant="success">Success Alert</b-alert>
+    <b-alert show variant="danger">Danger Alert</b-alert>
+    <b-alert show variant="warning">Warning Alert</b-alert>
+    <b-alert show variant="info">Info Alert</b-alert>
+    <b-alert show variant="light">Light Alert</b-alert>
+    <b-alert show variant="dark">Dark Alert</b-alert>
+
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Books library'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+/* Material Design with Vue and Bootstrap */
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/build/css/mdb.css';
+
+/* Bootstrap Vue */
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+/* Routes - Pages and Template Pages*/
+import Home from './components/Home'
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+
+import App from './App'
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path: '/', component: Home},
+  ]
+});
+
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
